447426d8c3f42e1f34e76215c88d6aeedb9e7a14
Willian Amancio <wvsamancio@gmail.com>
2023-05-07 17:22:31 -0300

    Testing getenv

--- src/main/java/backend/app/AppApplication.java
+++ src/main/java/backend/app/AppApplication.java
@@ -1,13 +1,16 @@
 package backend.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
+    @Value("${spring.data.mongodb.database}")
+    private static String databaseName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+		System.out.println("Database name: " + databaseName);
 	}
-
 }
