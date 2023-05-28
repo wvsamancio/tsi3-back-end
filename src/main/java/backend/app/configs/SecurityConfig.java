4891a39660bc95d0dd815f08659433284e72d62f
Willian Amancio <wvsamancio@gmail.com>
2023-05-28 12:55:44 -0300

    security test

--- src/main/java/backend/app/configs/SecurityConfig.java
+++ src/main/java/backend/app/configs/SecurityConfig.java
@@ -16,32 +16,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
-                // // COMMON - GET
-                // .requestMatchers(HttpMethod.GET, "/sign").hasAnyRole("USER", "ADMIN")
-                // // USER - GET
-                // .requestMatchers(HttpMethod.GET, "/contributions/{username}").hasRole("USER")
-                // .requestMatchers(HttpMethod.GET,
-                // "/contributions/{username}/{id}").hasRole("USER")
-                // // USER - POST
-                // .requestMatchers(HttpMethod.POST, "/contributions").hasRole("USER")
-                // // ADMIN - GET
-                // .requestMatchers(HttpMethod.GET, "/contributions").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.GET,
-                // "/contributions/{username}/{id}").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
-                // // ADMIN - POST
-                // .requestMatchers(HttpMethod.POST, "/contacts").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.POST, "/infos").hasRole("ADMIN")
-                // // ADMIN - DELETE
-                // .requestMatchers(HttpMethod.DELETE, "/contacts/{id}").hasRole("ADMIN")
-                // .requestMatchers(HttpMethod.DELETE, "/infos/{id}").hasRole("ADMIN")
-                // // ALL - GET
-                // .requestMatchers(HttpMethod.GET, "/contacts").permitAll()
-                // .requestMatchers(HttpMethod.GET, "/infos").permitAll()
-                // .requestMatchers(HttpMethod.GET, "/infos/{id}").permitAll()
-                // // ALL - POST
-                // .requestMatchers(HttpMethod.POST, "/users").permitAll());
-                .anyRequest().permitAll());
+                // COMMON - GET
+                .requestMatchers(HttpMethod.GET, "/sign").hasAnyRole("USER", "ADMIN")
+                // USER - GET
+                .requestMatchers(HttpMethod.GET, "/contributions/{username}").hasRole("USER")
+                .requestMatchers(HttpMethod.GET, "/contributions/{username}/{id}").hasRole("USER")
+                // USER - POST
+                .requestMatchers(HttpMethod.POST, "/contributions").hasRole("USER")
+                // ADMIN - GET
+                .requestMatchers(HttpMethod.GET, "/contributions").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/contributions/{username}/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                // ADMIN - POST
+                .requestMatchers(HttpMethod.POST, "/contacts").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/infos").hasRole("ADMIN")
+                // ADMIN - DELETE
+                .requestMatchers(HttpMethod.DELETE, "/contacts/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/infos/{id}").hasRole("ADMIN")
+                // ALL - GET
+                .requestMatchers(HttpMethod.GET, "/contacts").permitAll()
+                .requestMatchers(HttpMethod.GET, "/infos").permitAll()
+                .requestMatchers(HttpMethod.GET, "/infos/{id}").permitAll()
+                // ALL - POST
+                .requestMatchers(HttpMethod.POST, "/users").permitAll());
 
         http.csrf(csrf -> csrf.disable());
         http.httpBasic(withDefaults());
