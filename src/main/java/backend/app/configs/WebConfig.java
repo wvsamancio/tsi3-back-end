4eef48e2cc0b48acf65c4d8e66e7d08078c37edc
Willian Amancio <wvsamancio@gmail.com>
2023-05-28 20:17:08 -0300

    teste

--- src/main/java/backend/app/configs/WebConfig.java
+++ src/main/java/backend/app/configs/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowedOrigins("*")
-                .allowCredentials(true);
+                .allowedOrigins("*");
     }
 }
