90f5590da35f114d30e4dbe55be473767cbd9069
Willian Amancio <wvsamancio@gmail.com>
2023-05-28 20:10:45 -0300

    teste

--- src/main/java/backend/app/configs/WebConfig.java
+++ src/main/java/backend/app/configs/WebConfig.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
     }
 }
