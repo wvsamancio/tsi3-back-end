f253545149afa0e022b27ce7df64763d201484e5
Willian Amancio <wvsamancio@gmail.com>
2023-05-28 16:27:44 -0300

    security test

--- src/main/java/backend/app/configs/SecurityConfig.java
+++ src/main/java/backend/app/configs/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
 
         http.csrf(csrf -> csrf.disable());
         http.cors(cors -> cors.disable());
+        http.headers(headers -> headers.disable());
         http.httpBasic(withDefaults());
         return http.build();
     }
